20ad5dfeba33a94bda5dcf9919b6f703656717a8
Vitalii Kostyreva <vitalikys87@i.ua>
2022-09-01 17:47:36 +0200

    add author mod

--- author/urls.py
+++ author/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', list_author, name='list_author'),
     # path('add_author/', CreateAuthor.as_view(), name = 'add_author')
-    path('add_author/', add_author, name = 'add_author')
+    path('add_author/', add_author, name = 'add_author'),
+    path('delete_author/<int:id>', delete_author, name = 'author_delete_url'),
 
 ]
--- author/views.py
+++ author/views.py
@@ -1,8 +1,12 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 
 from .models import Author
 from .forms import AddAuthorForm
+
+@login_required(login_url='login_url')
 def list_author(request):
     return render(request, 'author/author_list.html', {'author': Author.get_all() } )
 
@@ -10,14 +14,41 @@ def add_author(request):
     if request.method == 'POST':
         form= AddAuthorForm(request.POST) # забираємо дані
         if form.is_valid():
-            form.save()
-            return redirect('list_author')
+            name = request.POST.get('name')
+            surname = request.POST.get('surname')
+            patronymic = request.POST.get('patronymic')
+            try:
+                Author.create(name, surname, patronymic)
+                messages.success(request, f'Add new author: {name} success !')
+                return redirect('list_author')
+            except Exception:
+                messages.error(request, 'Error creation author... щось пішло не так')
     else:
         form = AddAuthorForm() #створ екземпл пустої форми
     return render(request, 'author/add_author.html', {'form':form} )
 
+
+
+'''
+# def add_author(request):
+#     if request.method == 'POST':
+#         form= AddAuthorForm(request.POST) # забираємо дані
+#         if form.is_valid():
+#             form.save()
+#             return redirect('list_author')
+#     else:
+#         form = AddAuthorForm() #створ екземпл пустої форми
+#     return render(request, 'author/add_author.html', {'form':form} )
+
+'''
+@login_required(login_url='login_url')
+def delete_author(request, id):
+    Author.delete_by_id(id)
+    messages.success(request, 'You have delete author. Success !')
+    return redirect('list_author')
+
 # class CreateAuthor(CreateView):
 #     form_class = AddAuthorForm
-    template_name = 'author/add_author.html'
+#     template_name = 'author/add_author.html'
     # login_url = 'admin/'  # redirect if not authorized
     # raise_exception = True   # exeption = if not authorized
