61804fd684d2c69c1f24e1b579dd0ab681efd2f2
Vitalii Kostyreva <69001084+Vitalikys@users.noreply.github.com>
2022-09-12 16:57:12 +0300

    update settings heroku
    update settings heroku coment

--- library/settings.py
+++ library/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -190,7 +190,7 @@ LOGGING = {
     },
 }
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 try:
     from .local_settings import *
 except ImportError:
