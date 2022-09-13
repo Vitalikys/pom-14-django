2177f9f852ef8cfdb129b4489a511ead4f80b1c2
Vitalii Kostyreva <69001084+Vitalikys@users.noreply.github.com>
2022-09-14 00:13:07 +0300

    update forms order

--- order/forms.py
+++ order/forms.py
@@ -9,12 +9,15 @@ from order.models import Order
 class OrderForm(forms.ModelForm):
     # user = forms.ChoiceField(choices=CustomUser.get_all(), label='Користувач:')
     class Meta:
+        date_today = datetime.date.today
+        end_date = datetime.date.today() + datetime.timedelta(days=14)
         model = Order
-        fields = ['book','plated_end_at','user'] #, ,
+        fields = ['book','plated_end_at'] #, ,,'user'
         widgets = {
             'book': forms.Select(attrs={'class': 'form-control'}),
-            'user': forms.RadioSelect(),
+            # 'user': forms.RadioSelect(),
             # 'user': forms.HiddenInput(),
-            'plated_end_at': forms.DateInput(attrs={'type': 'date'}),
+            'plated_end_at': forms.DateInput(attrs={'type': 'date', 'min':date_today, 'max':end_date}),
         }
 
+        
